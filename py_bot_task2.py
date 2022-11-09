c92b014f7de1dc102f5f793c435eaab28d158547
Squirrel <vinchasmagic@gmail.com>
2022-11-09 11:07:03 +0300

    les 2

--- py_bot_task2.py
+++ py_bot_task2.py
@@ -5,25 +5,38 @@ show - напечатать все добавленные задачи.'''
 
 tasks = {
 
-
 }
 
-#Сегодня, завтра, 31.12 ...
-#[Задача 1, Задача2, Задача3]
-#Дата -> [Задача 1, Задача2, Задача3]
+# Сегодня, завтра, 31.12 ...
+# [Задача 1, Задача2, Задача3]
+# Дата -> [Задача 1, Задача2, Задача3]
 
 run = True
 
 while run:
-    command = input("Введите команду:\n")
+    command = input("Введите команду: ")
     if command == "help":
         print(HELP)
     elif command == "show":
-        print(tasks)
-    elif command == "add":
-        date = input("Введите дату для добавления задачи:\n")
-        tasks = input("Введите название задачи:\n")
+        date = input("Введите дату для отображения списка задач: ")
         if date in tasks:
+           for task in tasks[date]:
+               print('_ ', task)
+           else:
+               print("Такой даты нет!")
+    elif command == "add":
+        date = input("Введите дату для добавления задачи: ")
+        task = input("Введите название задачи:\n")
+        if date in tasks:
+            # Дата есть в словаре
+            # Добавляем в список задачу
             tasks[date].append(task)
-
-
+        else:
+            # Даты в словаре нет
+            # Создаем запись с ключом date
+            tasks[date] = []
+            tasks[date].append(task)
+        print("Задача", tasks, "добавлена на дату", date)
+    else:
+     print("Неизвестная команда!")
+     break
